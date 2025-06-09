821da1eb68981b1ab32ea9486f1bf8edf64c6536
Kateryna Tsyhaniuk <kyzkakatryska@gmail.com>
2025-06-09 13:46:10 +0300

    create my server

---
+++ src/server.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import nodemailer from 'nodemailer';
+
+dotenv.config();
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+app.post('/send-email', async (req, res) => {
+  const { email, comment } = req.body;
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: process.env.EMAIL_TO,
+    subject: 'New Form Submission',
+    text: `📬 New form submission:\n\nEmail: ${email}\nComment: ${comment}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res
+      .status(200)
+      .json({ title: 'Success!', message: 'Message sent successfully.' });
+  } catch (error) {
+    console.error('Error sending email:', error);
+    res.status(500).json({ title: 'Error', message: 'Failed to send email.' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
